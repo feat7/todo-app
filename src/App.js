eee67c76d2a0ae69c4bfeacc850003378f96550b
feat7 <vinaykhobragade99@gmail.com>
2017-07-18 00:24:43 +0530

    flatlist

--- src/App.js
+++ src/App.js
@@ -5,13 +5,45 @@ import {
   View,
   Text,
   StyleSheet,
+  FlatList
 } from 'react-native';
 
 export default class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      todoList: [
+        {
+          id: 1,
+          title: "This is title one!"
+        },
+        {
+          id: 2,
+          title: "This is title two!"
+        },
+        {
+          id: 3,
+          title: "This is title three!"
+        }
+      ]
+    }
+
+  }
+
+  _renderItem = ({item}) => (
+    <Text>{item.title}</Text>
+  )
+
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>App.js</Text>
+        <FlatList
+          data={this.state.todoList}
+          keyExtractor={item => item.id}
+          renderItem={this._renderItem}
+        />
       </View>
     );
   }
